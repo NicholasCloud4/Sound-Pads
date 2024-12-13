90a21b1a81acd0c29f827d9da2c793ef3b47209e
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-13 13:39:33 -0500

    Turning buttons on or off

--- Pad.jsx
+++ Pad.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default function Pad(props) {
+    /**
+     * Challenge part 3:
+     * Our buttons got turned off by default! Update the code
+     * so if the button is "on", it has the className of "on".
+     */
+
     return (
         <button
             style={{ backgroundColor: props.color }}
+            className={props.on === true ? "on" : ""}
         ></button>
     )
 }
