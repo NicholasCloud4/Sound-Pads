18acb5b45929219a55ba6bcd30f93bfedbed95e1
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-13 13:30:15 -0500

    Changed the button color

--- App.jsx
+++ App.jsx
@@ -1,28 +1,40 @@
 import { useState } from "react";
 import padsData from "./pads";
+import Pads from "./pads";
+import Pad from "./Pad";
 
-export default function App({ darkMode }) {
+export default function App() {
     const [pads, setPads] = useState(padsData);
-
-    /**
-     * Challenge: use a ternary to determine the backgroundColor
-     * of the buttons
-     * If darkMode is true, set them to "#222222"
-     * If darkMode is false, set them to "#cccccc"
-     */
 
     const buttonElements = pads.map((pad) => {
         return (
-            <button
-                style={
-                    darkMode === true
-                        ? { backgroundColor: "#222222" }
-                        : { backgroundColor: "#cccccc" }
-                }
-                key={pad.id}
-            ></button>
+
+            <Pad key={pad.id} color={pad.color} />
+            // <button
+            //     style={
+            //         darkMode === true
+            //             ? { backgroundColor: "#222222" }
+            //             : { backgroundColor: "#cccccc" }
+            //     }
+            //     key={pad.id}
+            // ></button>
         );
     });
+
+
+    /**
+     * Challenge part 2:
+     * 1. Create a separate component called "Pad" and
+     *    replace the `button` above with our <Pad /> component
+     * 
+     * 2. Pass the Pad component a prop called `color` with the
+     *    value of the same name from the `padsData` objects
+     * 
+     * 3. In the Pad component, apply an inline style to the <button>
+     *    to set the backgroundColor of the button.
+     * 
+     * (We'll deal with the "on" property soon)
+     */
     return (
         <main>
             <div className="pad-container">{buttonElements}</div>
---
+++ Pad.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Pad(props) {
+    return (
+        <button
+            style={{ backgroundColor: props.color }}
+        ></button>
+    )
+}
