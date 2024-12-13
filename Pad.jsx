5f1285d13b3632b268c6b1c2862f4ea246326264
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-13 14:10:22 -0500

    Local state updating the button

--- Pad.jsx
+++ Pad.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Pad(props) {
     /**
-     * Challenge part 3:
-     * Our buttons got turned off by default! Update the code
-     * so if the button is "on", it has the className of "on".
-     */
+    * Challenge: Create state controlling whether
+    * this pad is "on" or "off". Use the incoming
+    * `props.on` to determine the initial state.
+    * 
+    * Create an event listener so when the pad is clicked,
+    * it toggles from "on" to "off".
+    * 
+    * Goal: clicking each pad should toggle it on and off.
+    */
+
+    const [padStatus, setPadStatus] = useState(props.on);
+    console.log(padStatus)
+
+    function handleClick() {
+        setPadStatus((prevStatus) => {
+            return !prevStatus
+        })
+    }
 
     return (
         <button
             style={{ backgroundColor: props.color }}
-            className={props.on === true ? "on" : ""}
+            className={padStatus === true ? "on" : ""}
+            onClick={handleClick}
         ></button>
     )
 }
